47570a2523e1e5b33fe23ede1a6079cdf7c414b7
Kelvin Chan <kelvinkaifungchan@gmail.com>
2022-02-19 23:44:44 +0000

    admin router

--- backend/routers/adminRouter.js
+++ backend/routers/adminRouter.js
@@ -9,8 +9,12 @@ class AdminRouter {
     router() {
         let router = express.Router();
         router.post("/", this.post.bind(this))
+        router.post("/tag", this.postTag.bind(this))
+        router.post("/attachment", this.postAttachment.bind(this))
         router.put("/", this.put.bind(this))
-        router.post("/delete", this.delete.bind(this))
+        router.post("/deletearticle", this.delete.bind(this))
+        router.post("/deletetag", this.delete.bind(this))
+        router.post("/deleteattachment", this.delete.bind(this))
 
         return router
     }
@@ -31,21 +35,33 @@ class AdminRouter {
                     .addAttachment(req.body.articleId, attachment.attachmentLink)
                 })
             })
-            .then((data) => {
-                return res.json(data)
-            })
             .catch((err) => {
                 return res.status(500).json(err)
             })
+    }
+
+    postTag(req, res) {
+        console.log("Requesting adding tag to article")
+        return this.bridgeService
+        .addTag(req.body.articleId, req.body.tag)
+        .catch((err) => {
+            return res.status(500).json(err)
+        })
+    }
+
+    postAttachment(req, res) {
+        console.log("Requesting adding attachment to article")
+        return this.bridgeService
+        .addAttachment(req.body.articleId, req.body.attachmentLink)
+        .catch((err) => {
+            return res.status(500).json(err)
+        })
     }
 
     put(req, res) {
         console.log("Requesting editing article")
         return this.articleService
             .edit(req.body)
-            .then((data) => {
-                return res.json(data)
-            })
             .catch((err) => {
                 return res.status(500).json(err)
             })
@@ -53,6 +69,24 @@ class AdminRouter {
 
     delete(req, res) {
     }
+
+    deleteTag(req, res) {
+        console.log("Requesting deleting tag from article")
+        return this.bridgeService
+        .deleteTag(req.body.articleId, req.body.tag)
+        .catch((err) => {
+            return res.status(500).json(err)
+        })
+    }
+
+    deleteAttachment(req, res) {
+        console.log("Requesting deleting tag from article")
+        return this.bridgeService
+        .deleteAttachment(req.body.articleId, req.body.attachmentLink)
+        .catch((err) => {
+            return res.status(500).json(err)
+        })
+    }
 }
 
-module.exports = SetRouter;
+module.exports = AdminRouter;
