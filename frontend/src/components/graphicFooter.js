dc665b24f928b8a6e8f66f52ebc4e4a565c61855
ih810 <ivanhong810@gmail.com>
2022-02-26 23:56:36 +0800

    react component base

---
+++ frontend/src/components/graphicFooter.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const GraphicFooter = () => {
+
+};
+
+export default GraphicFooter;
---
+++ frontend/src/components/ticker.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ticker = () => {
+
+}
+
+export default ticker;
