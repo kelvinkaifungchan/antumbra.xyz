186b8e89a392a947e0ce1a4555b21c85d4b1b8c8
Kelvin Chan <kelvinkaifungchan@gmail.com>
2022-03-05 11:59:26 +0000

    updated components

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -2,6 +2,27 @@ import React from 'react';
 
 const Navbar = () => {
 
+
+
+return (
+    <div>
+        <ul>
+            <li> 
+                <Link to="/">44</Link>
+            </li>
+            <li> 
+                <Link to="/archive">Archive</Link>
+            </li>
+            <li> 
+                <Link to="/contact">Contact</Link>
+            </li>
+            <li> 
+                <Link to="/contact">Call For Media</Link>
+            </li>
+        </ul>
+    </div>
+);
+
 };
 
 export default Navbar;
