5c0f123c37d00c83ce4d7d9eb85725c97e66f725
Kelvin Chan <kelvinkaifungchan@gmail.com>
2022-03-06 16:30:02 +0000

    basic navbar

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -4,21 +4,24 @@ import "./navbar.module.css"
 
 export const NavBar = () => {
 return (
-<nav>
-    <ul>
-        <li>
-            <Link to="/">44</Link>
-        </li>
-        <li>
-            <Link to="/contact">Archive</Link>
-        </li>
-        <li>
-            <Link to="/contact">Contact</Link>
-        </li>
-        <li style={{float:"right"}}>
-            <Link to="/contact">Call for Media</Link>
-        </li>
-    </ul>
+<nav className='p-3'>
+    <div className='d-flex justify-content-between'>
+        <div>
+            <b><Link to="/">44</Link></b>
+        </div>
+        <div>
+            <Link className="px-3" to="/archive">Archive</Link>
+            <Link className="px-3" to="/contact">Contact</Link>
+            <Link className="pl-3" to="/contact">Call for Media</Link>
+        </div>
+    </div>
+    <hr
+        style={{
+            color: 'white',
+            backgroundColor: 'white',
+            height: 0.5,
+        }}
+    />
 </nav>
 )
 }
