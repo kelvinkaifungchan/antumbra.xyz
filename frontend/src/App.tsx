8563df579717abc68b343c19c8784e663c487425
Kelvin Chan <kelvinkaifungchan@gmail.com>
2022-03-06 13:49:03 +0000

    added home route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,33 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Home } from './pages/Home';
+import { Contact } from './pages/Contact';
 import {
   BrowserRouter as Router,
-  Route
+  Route,
+  Routes
 } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-      </header>
-      {/* <Home/>
       <Router>
-          <Route path="/"/>
-      </Router> */}
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/contact" element={<Contact />}/>
+          </Routes>
+      </Router>
 
     </div>
   );
--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const NavBar = () => {
     return (
         <div>
-            Hi There
+                <li> 
+                    <button><Link to="/">44</Link></button>
+                </li>
+                <li> 
+                    <button><Link to="/contact">Archive</Link></button>
+                </li>
+                <li> 
+                    <button><Link to="/contact">Contact</Link></button>
+                </li>
+                <li> 
+                    <button><Link to="/contact">Call for Media</Link></button>
+                </li>
         </div>
     )
 }
