137e6c576719ad6766cb6928c9bc2acc1f9585b8
ih810 <ivanhong810@gmail.com>
2022-03-13 17:05:46 +0800

    homepage header

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { NavBar } from '../components/navbar';
+import styles from './Home.module.css';
+import './Home.module.css';
 
 export const Home = () => {
     return (
         <div>
+                <div className={`${styles.blur} ${styles.dflexCenter}`}>
+                    AARCHITECTURE
+                </div>
+                <div className={`${styles.header} ${styles.dflexCenter}`}>
+                    <h1>THE WARM WAR</h1>
+                </div>
             <NavBar/>
-            Hi There
         </div>
     )
 }
