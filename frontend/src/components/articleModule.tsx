7585e4ced154e646a06a47de77aeb586d5d67fb2
ih810 <ivanhong810@gmail.com>
2022-03-13 23:30:24 +0800

    article module

--- frontend/src/components/articleModule.tsx
+++ frontend/src/components/articleModule.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-export const ArticleModule = () => {
+interface Props {
+    img: string;
+    content: string;
+}
+
+export const ArticleModule = (props: Props) => {
     return (
-        <div>
-            Hi There
+        <div style={{ backgroundImage:`url("${props.img}")`, width:'auto', height:'100%', backgroundRepeat:'no-repeat', backgroundSize:'auto'}}>
+            <h2 className='p-3'>{props.content}</h2>
         </div>
     )
 }
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import { NavBar } from "../components/navbar";
 import styles from "./Home.module.css";
 import "./Home.module.css";
+import img from "../assets/meme13.jpeg";
 import Ticker from "react-ticker";
 import { Chip } from "@mui/material";
+import { HorizontalLine } from "../components/horizontalLine";
+import { ArticleModule } from "../components/articleModule";
 
 const tagList = [
   "Infrastucture",
@@ -13,6 +16,17 @@ const tagList = [
   "Others",
   "Architecture",
 ];
+
+const articleList = [
+  "Colonial Infrastructures and Technosocial Networks",
+  "Colonial Infrastructures and Technosocial Networks",
+  "Colonial Infrastructures and Technosocial Networks",
+  "Colonial Infrastructures and Technosocial Networks",
+  "Colonial Infrastructures and Technosocial Networks",
+  "Colonial Infrastructures and Technosocial Networks",
+];
+
+const imageLink = ``
 export const Home = () => {
   return (
     <div>
@@ -34,13 +48,33 @@ export const Home = () => {
           )}
         </Ticker>
       </div>
+      <HorizontalLine />
       {/* tags */}
       <div className={styles.tags}>
         {tagList.map((tagItem, index) => {
-          return <Chip label={tagItem} variant="outlined" sx={{color:'#FF5C00', border:'1px solid #FF5C00'}} />;
+          return (
+            <Chip
+            key={index}
+              label={tagItem}
+              variant="outlined"
+              sx={{ color: "#FF5C00", border: "1px solid #FF5C00", mr: 2 }}
+            />
+          );
         })}
       </div>
+      <HorizontalLine />
       {/* articles */}
+      <div className="container m-0" style={{maxWidth:'100%'}}>
+        <div className="row">
+          {articleList.map((item, index)=>{
+              return (
+                  <div key={index} className={`${styles.articleGrid} col col-sm-6 mb-3`}>
+                    <ArticleModule content={item} img={img}/>
+                  </div>
+                  )
+                })}
+        </div>
+      </div>
     </div>
   );
 };
