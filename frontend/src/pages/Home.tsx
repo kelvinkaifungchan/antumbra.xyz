38ea087024f9521fddc8cdd4069aaf961213913a
ih810 <ivanhong810@gmail.com>
2022-03-13 17:54:46 +0800

    tags

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,11 +2,21 @@ import React from "react";
 import { NavBar } from "../components/navbar";
 import styles from "./Home.module.css";
 import "./Home.module.css";
-import Ticker from 'react-ticker'
+import Ticker from "react-ticker";
+import { Chip } from "@mui/material";
+
+const tagList = [
+  "Infrastucture",
+  "Memes",
+  "Architecture",
+  "Others",
+  "Others",
+  "Architecture",
+];
 export const Home = () => {
   return (
     <div>
-        {/* header */}
+      {/* header */}
       <div className={`${styles.blur} ${styles.dflexCenter}`}>
         AARCHITECTURE
       </div>
@@ -15,18 +25,20 @@ export const Home = () => {
       </div>
       <NavBar />
       {/* scroll bar ticker*/}
-      <div>
+      <div className={styles.ticker}>
         <Ticker>
-        {({ index }) => (
+          {({ index }) => (
             <>
-                <h1>This is the Headline of element #{index}!</h1>
+              <h1>This is the Headline of element #{index}!</h1>
             </>
-        )}
+          )}
         </Ticker>
       </div>
       {/* tags */}
-      <div>
-
+      <div className={styles.tags}>
+        {tagList.map((tagItem, index) => {
+          return <Chip label={tagItem} variant="outlined" sx={{color:'#FF5C00', border:'1px solid #FF5C00'}} />;
+        })}
       </div>
       {/* articles */}
     </div>
