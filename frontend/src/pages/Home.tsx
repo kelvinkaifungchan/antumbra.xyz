12e2f40dcbd6207aec3222fa18e40682fce11dbb
ih810 <ivanhong810@gmail.com>
2022-03-13 17:42:40 +0800

    ticker placeholder

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,18 +1,34 @@
-import React from 'react';
-import { NavBar } from '../components/navbar';
-import styles from './Home.module.css';
-import './Home.module.css';
-
+import React from "react";
+import { NavBar } from "../components/navbar";
+import styles from "./Home.module.css";
+import "./Home.module.css";
+import Ticker from 'react-ticker'
 export const Home = () => {
-    return (
-        <div>
-                <div className={`${styles.blur} ${styles.dflexCenter}`}>
-                    AARCHITECTURE
-                </div>
-                <div className={`${styles.header} ${styles.dflexCenter}`}>
-                    <h1>THE WARM WAR</h1>
-                </div>
-            <NavBar/>
-        </div>
-    )
-}
+  return (
+    <div>
+        {/* header */}
+      <div className={`${styles.blur} ${styles.dflexCenter}`}>
+        AARCHITECTURE
+      </div>
+      <div className={`${styles.header} ${styles.dflexCenter}`}>
+        <h1>THE WARM WAR</h1>
+      </div>
+      <NavBar />
+      {/* scroll bar ticker*/}
+      <div>
+        <Ticker>
+        {({ index }) => (
+            <>
+                <h1>This is the Headline of element #{index}!</h1>
+            </>
+        )}
+        </Ticker>
+      </div>
+      {/* tags */}
+      <div>
+
+      </div>
+      {/* articles */}
+    </div>
+  );
+};
