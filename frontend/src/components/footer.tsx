2f6db4a9c7beb2b1dbdc9678f256477f70ba4eaa
Kelvin Chan <kelvinkaifungchan@gmail.com>
2022-05-15 21:37:06 +0100

    updated navbar

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -27,7 +27,7 @@ return (
                 <a href='https://www.aaschool.ac.uk/' target={"blank"}><img src="https://aarchitecture.blob.core.windows.net/videos/Architectural_association_school_of_architecture_logo.png" alt="Architectural Association" width="auto" height="60"/></a>
         </div>
         <div className='col-12 col-md-6 col-lg-3' style={{fontSize:'1rem', opacity:'0.7', width:"20%", textAlign:'end'}}>
-        The Warm War is developed and edited by Karl Herdersch, Kaifung Kelvin Chan and Alexander Balgarnie
+        Penumbra is developed and edited by Karl Herdersch, Kaifung Kelvin Chan and Alexander Balgarnie
         </div>
         <div className='col-12 col-md-6 col-lg-3' style={{fontSize:'1rem'}}>
             <Link className="px-3" to="/callformedia">Call for Media</Link><br/>
@@ -35,7 +35,7 @@ return (
             <Link className="px-3" to="/archive">Archive</Link><br/>
         </div>
         <div className='col-12 col-md-6 col-lg-3'style={{fontSize:'1rem', textAlign:'end'}}>
-            <a href="mailto:aarchitecture@aaschool.ac.uk">Contribute</a><br/>
+            <a href="mailto:aarchitecture@aaschool.ac.uk">Participate</a><br/>
             <a href="https://www.instagram.com/aarchitecture44/" target={"blank"}>Instagram</a><br/>
         </div>
     </div>
--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -8,20 +8,20 @@ return (
     <nav className={`px-3 pt-3 ${styles.screenBig}`}>
         <div className="d-flex justify-content-between" style={{color: "#FF5C00", fontSize: "1.8rem"}}>
             <div>
-                <Link to="/"><span style={{filter: 'blur(3px)'}}>AARCHITECTURE</span> 44</Link>
+                <Link to="/"><span style={{filter: 'blur(3px)'}}>PENUMBRA</span> Warmth</Link>
             </div>
-            <div>
-                <Link to="/callformedia">The Warm War</Link>
-            </div>
+            {/* <div>
+                <Link to="/callformedia">Warmth</Link>
+            </div> */}
         </div>
     </nav>
     <nav className={`px-3 pt-3 ${styles.screenSmall}`}>
         <div className="d-flex justify-content-between" style={{color: "#FF5C00", fontSize: "1.8rem"}}>
             <div>
-                <Link to="/"><span style={{filter: 'blur(3px)'}}>AA</span> 44</Link>
+                <Link to="/"><span style={{filter: 'blur(3px)'}}>PENUMBRA</span></Link>
             </div>
             <div>
-                <Link to="/callformedia">The Warm War</Link>
+                <Link to="/callformedia">Warmth</Link>
             </div>
         </div>
     </nav>
